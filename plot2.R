b0addbb87a59f3ea3a6f76268cdee28e3707f482
anadirisal <anadirisal@gmail.com>
2016-10-16 17:20:44 -0400

    These are 4 code files and 4 graphs related to them.

---
+++ plot2.R
@@ -0,0 +1,12 @@
+## Reads the datafile
+
+dataFile <- "~/Downloads/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+date_time <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(as.character(subSetData$Global_active_power))
+png("~/course5/Project1/plot2.png", width=480, height=480)
+plot(date_time, globalActivePower, type = "l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
